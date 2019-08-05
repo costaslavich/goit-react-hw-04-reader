94e4a817f767f2e7219e460300a30c7aa1ec849f
Konstantin Chernykh <costaslavich@gmail.com>
2019-08-05 07:56:19 +0300

    Updates

---
+++ precache-manifest.ff41de00df0929e01b4bb608b54ac1dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63371e124cfabfc4b6151ab67a61a8d2",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "9bc8b027723b51af0085",
+    "url": "/goit-react-hw-04-reader/static/css/main.e12a1eb7.chunk.css"
+  },
+  {
+    "revision": "5d6701f83b4816de6529",
+    "url": "/goit-react-hw-04-reader/static/js/2.e968be9a.chunk.js"
+  },
+  {
+    "revision": "9bc8b027723b51af0085",
+    "url": "/goit-react-hw-04-reader/static/js/main.98bb560e.chunk.js"
+  },
+  {
+    "revision": "84bf89e08b01550b9b7d",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.860f1cd1.js"
+  }
+]);
