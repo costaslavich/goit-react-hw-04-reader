779d3f3f7f568f4baaa1802c4b9756a8bd22c0af
Konstantin Chernykh <costaslavich@gmail.com>
2019-08-05 07:54:57 +0300

    react-hw04-2

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-04-reader/precache-manifest.a98234fd8981fe62b9370a08d7f139ba.js"
+  "/goit-react-hw-04-reader/precache-manifest.ff41de00df0929e01b4bb608b54ac1dd.js"
 );
 
 self.addEventListener('message', (event) => {
