ebac17f1767a46a0296c3ab87f824252df942be6
Konstantin Chernykh <costaslavich@gmail.com>
2019-08-12 18:06:32 +0300

    different mistake about index url

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-04-reader/precache-manifest.ff41de00df0929e01b4bb608b54ac1dd.js"
+  "/goit-react-hw-04-reader/precache-manifest.fcf26c68e86d3c0228361ac625e9444e.js"
 );
 
 self.addEventListener('message', (event) => {
